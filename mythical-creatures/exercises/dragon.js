e14a5aaae2429ba19972643fd092e695d5df2cbb
Carly Collums <carlycollums@gmail.com>
2021-09-01 15:45:47 -0600

    Add dragon implementation

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,31 @@
 
+
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.eating = 0;
+   }
+   greet() {
+     return `Hi, ${this.rider}!`;
+   }
+
+   eat() {
+     this.eating ++;
+    if (this.eating === 3) {
+      return this.hungry = false;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+module.exports = Dragon;
