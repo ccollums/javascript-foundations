90146912272d3824f2782d7224130e80cc16784c
Carly Collums <carlycollums@gmail.com>
2021-09-07 21:32:14 -0600

    Adds completed medusa test

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,28 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim) {
+  var scaredStatue = new Statue(victim.name)
+  if (this.statues.length <= 2) {
+    this.statues.push(scaredStatue);
+  } else if (this.statues.length >= 3) {
+    var releasedVictim = new Person(this.statues[0].name)
+    this.statues.shift();
+    this.statues.push(scaredStatue);
+    releasedVictim.mood = 'relieved';
+    return releasedVictim;
+  }
+}
+}
+
+
+
+
+
+module.exports = Medusa;
