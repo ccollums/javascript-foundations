4a8fb1c58c0e15f0b2b12543e6610cdb604ca733
Carly Collums <carlycollums@gmail.com>
2021-09-11 11:21:04 -0600

    Adds completed bag test

---
+++ trick-or-treat/lib/bag.js
@@ -0,0 +1,27 @@
+class Bag {
+  constructor() {
+    this.empty = true;
+    this.count = 0;
+    this.candies = [];
+  }
+
+  fill(candy) {
+    this.candies.push(candy);
+    this.count++;
+  }
+
+  contains(candy) {
+  if (this.candies[0].type.includes(candy)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  }
+
+
+
+
+
+module.exports = Bag;
