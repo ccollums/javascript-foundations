5c5fef76424ffc9118e4603aab0bdfe7169aac49
Carly Collums <carlycollums@gmail.com>
2021-09-11 11:50:15 -0600

    Add more trick or treater tests

--- trick-or-treat/lib/trick-or-treater.js
+++ trick-or-treat/lib/trick-or-treater.js
@@ -1,11 +1,15 @@
 class TrickOrTreater {
   constructor(costume, bag) {
-    this.dressedUpAs = costume.style;git 
+    this.dressedUpAs = costume.style
+    this.bag = bag;
     this.hasCandy = false;
+    this.countCandies = 0;
   }
 
   putCandyInBag(candy) {
     this.hasCandy = true;
+    this.bag.candies.push(candy);
+    this.countCandies++;
   }
 }
 
