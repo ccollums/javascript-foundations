745e31b5f2378790d82a2f0099f9ef8fb83f61f9
Carly Collums <carlycollums@gmail.com>
2021-09-11 11:37:38 -0600

    Adds completion of first 3 trick or treater tests

---
+++ trick-or-treat/lib/trick-or-treater.js
@@ -0,0 +1,18 @@
+class TrickOrTreater {
+  constructor(costume, bag) {
+    this.dressedUpAs = costume.style;git 
+    this.hasCandy = false;
+  }
+
+  putCandyInBag(candy) {
+    this.hasCandy = true;
+  }
+}
+
+
+
+
+
+
+
+module.exports = TrickOrTreater;
