7acc7ef28146418ae2f82c6ddacf6ee4028481ae
Carly Collums <carlycollums@gmail.com>
2021-09-14 22:31:47 -0600

    Adds completion of magician tests

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,39 @@
+class Magician {
+  constructor({name, assistant, clothing}) {
+    this.name = 'The Great ' + name;
+    this.assistant = assistant;
+    this.favoriteAccessory = clothing || 'top hat';
+    this.confidencePercentage = 10;
+  }
+
+  performIncantation(chant) {
+    return `${chant.toUpperCase()}!`;
+  }
+
+  performTrick() {
+    this.confidencePercentage += 10;
+    if (this.favoriteAccessory === 'top hat') {
+      return 'PULL RABBIT FROM TOP HAT';
+    } else {
+      return 'PULL DOVE FROM SLEEVE';
+    }
+  }
+
+  performShowStopper() {
+    if (this.confidencePercentage <= 100 || !this.assistant) {
+      return 'Oh no, this trick is not ready!';
+    } else {
+      return 'WOW! The magician totally just sawed that person in half!';
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+module.exports = Magician;
