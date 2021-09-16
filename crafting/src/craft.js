e493be66b83865370296d42e8bec8a2dcb620318
Carly Collums <carlycollums@gmail.com>
2021-09-15 20:30:43 -0600

    Adds completed crafting tests

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,22 @@
 class Craft {
+  constructor({type, materials}) {
+    this.name = type
+    this.materials = materials;
+    this.completed = false;
+    this.cost = 0;
+  }
 
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!'
+  }
+
+  calculateProjectTotal() {
+    for (var i = 0; i < this.materials.length; i++) {
+      this.cost += this.materials[i].calculateMaterialCost();
+    }
+    return this.cost;
+  }
 }
 
 module.exports = Craft;
