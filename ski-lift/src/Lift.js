a9133d89ed13ff51c3a66cce0f8b31f2d1891675
Carly Collums <carlycollums@gmail.com>
2021-09-21 14:43:50 -0600

    Add completed ski lift tests

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,35 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+  constructor(limit) {
+    this.inService = true;
+    this.limit = limit;
+    this.skiers = [];
+    this.safetyBar = 'up';
+  }
+
+  admitSkier(name, skipass) {
+    var skier = new Skier(name);
+    skier.hasLiftTicket = skipass;
+    if (skier.hasLiftTicket === true && this.limit > this.skiers.length) {
+    this.skiers.push(skier);
+  } else if (skier.hasLiftTicket === true) {
+    return `Sorry, ${skier.name}. Please wait for the next lift!`;
+  } else {
+    return `Sorry, ${skier.name}. You need a lift ticket!`
+  }
+}
+
+startLift() {
+  var neededSkiers = this.limit-this.skiers.length
+  if (this.skiers.length === this.limit) {
+    this.safetyBar = 'down'
+  } else if (neededSkiers > 1){
+    return `We still need ${neededSkiers} more skiers!`
+  } else {
+    return `We still need ${neededSkiers} more skier!`
+  }
+}
 }
 
 module.exports = Lift;
