8fee9948d62f1bfa0481af6eae631f034abe19d6
Carly Collums <carlycollums@gmail.com>
2021-09-20 21:03:30 -0600

    Add completed runner tests

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,26 @@
 class Runner {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
 
+  runSomeMiles(miles) {
+    this.milesRun = this.milesRun+miles;
+    this.fitness = this.fitness+miles;
+  }
+
+  stretch() {
+    this.fitness+=.5
+  }
+
+  runRace(raceName, miles) {
+    this.completedRaces.push(raceName)
+    this.milesRun+=miles;
+    this.fitness+=miles
+  }
 }
 
 module.exports = Runner;
