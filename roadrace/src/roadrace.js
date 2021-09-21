c9d3941377d1dc07bc826cbedabf066603eaf8bc
Carly Collums <carlycollums@gmail.com>
2021-09-21 10:35:46 -0600

    Add passed road race tests

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,28 @@
 class Roadrace {
+  constructor({title, city}) {
+    this.name = title;
+    this.location = city;
+    this.distance = 0;
+    this.participants = [];
+  }
 
+  setDistance(miles) {
+    this.distance+=miles;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+
+  completeRace() {
+    for (var i = 0; i <this.participants.length; i++) {
+      if (this.participants[i].name && !this.participants[i].completedRaces.includes(this.name)) {
+        this.participants[i].completedRaces.push(this.name)
+        this.participants[i].milesRun+=this.distance;
+    }
+  }
+}
 }
 
 module.exports = Roadrace;
