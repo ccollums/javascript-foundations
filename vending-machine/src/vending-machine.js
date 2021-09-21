5666bcf76f9a06e3951b05b1fe8fa90e80a18092
Carly Collums <carlycollums@gmail.com>
2021-09-20 20:48:37 -0600

    Add first three tests of vending machine

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,21 @@
 class VendingMachine {
+  constructor({id, isBroken}) {
+    this.id = id;
+    this.isBroken = isBroken;
+    this.snacks = [];
+  }
 
+  addSnacks(snackName) {
+    // this.snacks.push(snackName);
+    // console.log(snackName.name)
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].name === snackName.name) {
+        return `Sorry, that snack is already stocked! Try adding a different snack.`
+  } else {
+    this.snacks.push(snackName);
+}
+}
+}
 }
 
 module.exports = VendingMachine;
