9acd22bfbf9fd987719f069883effe72d39365d9
Carly Collums <carlycollums@gmail.com>
2021-09-23 11:06:33 -0600

    Add completed lunchbox tests

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,27 @@
 class LunchBox {
+  constructor({owner, madeOf, shape, color}) {
+    this.owner = owner;
+    this.material = madeOf;
+    this.shape = shape;
+    this.color = color;
+    this.snacks = [];
+  }
 
-}
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    var healthySnack = [];
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy() === true) {
+        healthySnack.push(this.snacks[i].type);
+      }
+      }
+      return healthySnack;
+    }
+  }
+
 
 module.exports = LunchBox;
